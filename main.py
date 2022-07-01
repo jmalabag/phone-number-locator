0516ff796068262a724c4ae27af288eece2c6e5e
PersistThroughPain <jam2109@live.it>
2022-07-01 21:03:55 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from myphone import number
 import phonenumbers
-from phonenumbers import geocoder
-from phonenumbers import carrier
+from phonenumbers import geocoder, carrier, timezone
 import opencage
 from opencage.geocoder import OpenCageGeocode
 import folium
@@ -13,23 +12,25 @@ def show(basic_txt, additional_txt):
         print('Error in finding the ' + basic_txt)
 
 n = phonenumbers.parse(number)
-location = geocoder.description_for_number(n, 'en')
-show('City', location)
-
-service_pro = phonenumbers.parse(number)
-show('Service', carrier.name_for_number(service_pro, 'en'))
+city = geocoder.description_for_number(n, 'en')
+show('City', city)
+carrier = carrier.name_for_number(n, 'en')
+show('Carrier', carrier)
+timeZone = timezone.time_zones_for_number(n)
+print('Time Zone', str(timeZone)[2:-3])
 
 key = '@todo'
 geocoder = OpenCageGeocode(key)
-query = str(location)
+query = str(city)
 results = geocoder.geocode(query)
+#print(results)
 
 lat = results[0]['geometry']['lat']
 lng = results[0]['geometry']['lng']
-
 print('Latitude ', lat)
 print('Longitude ', lng)
 
 myMap = folium.Map(location=[lat, lng], zoom_start=9)
-folium.Marker([lat, lng], popup=location).add_to(myMap)
+folium.CircleMarker(location=[lat, lng], radius=50, popup=city).add_to(myMap)
+folium.Marker([lat, lng], popup=city).add_to(myMap)
 myMap.save("mylocation.html")
--- myphone.py
+++ myphone.py
@@ -1,3 +1,7 @@
 # phone number for testing
 # src: https://se.au/resources/test-phone-numbers/
-number = '+44 20 8759 9036'
+# https://howtophoneto.com/test/
+# https://stackoverflow.com/questions/54871005/global-phone-carrier-lookup-using-python
+#number = '+61291011948' ( no carrier found )
+#number = '+442087599036' ( no carrier found )
+number = '+40721234567'
